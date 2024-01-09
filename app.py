7d8ebc0388a247041ac33c752d29ffde3d5ecb4e
KarolKozlowski22 <kozlowskikarol02@gmail.com>
2024-01-09 13:44:31 +0100

    New endpoints for login and register features

--- app.py
+++ app.py
@@ -11,5 +11,13 @@ app.config['STATIC_AUTO_RELOAD'] = True
 def main():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
